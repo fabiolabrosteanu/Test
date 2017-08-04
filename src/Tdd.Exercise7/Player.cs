aa1c387fe848b8fd77b7641d6221f7eee3c9b613
Fabiola Brosteanu <fabiola.brosteanu@red-gate.com>
2017-08-04 13:50:50 +0100

    branch modif

--- src/Tdd.Exercise7/Player.cs
+++ src/Tdd.Exercise7/Player.cs
@@ -5,7 +5,7 @@ namespace Tdd.Exercise7
     public class Player : IPlayer
     {
         public Hand RevealHand()
-        {
+        {//branch modif
             Array values = Enum.GetValues(typeof(Hand));
             Random random = new Random();
            return (Hand)values.GetValue(random.Next(values.Length));
